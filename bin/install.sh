ad0d53354647e09531544c1fcaff7fd113647068
sbeller <sammbeller@gmail.com>
2015-11-30 14:37:34 -0500

    download and grab sms-tools

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+TMP_DIR="/tmp/sample_synth"
+
+## Download sms-tools
+echo "Downloading sms-tools"
+mkdir $TMP_DIR
+(cd $TMP_DIR && curl -LO "https://github.com/MTG/sms-tools/archive/master.zip")
+unzip -q ${TMP_DIR}/master.zip -d $TMP_DIR
+cp -R ${TMP_DIR}/sms-tools-master ./lib/sms-tools
+rm -rf $TMP_DIR
+
 if hash virtualenv 2>/dev/null; then
   echo "virtualenv available"
 else
