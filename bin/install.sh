4811ac9a225639bca0d49af2da96efdc80b067e9
sbeller <sammbeller@gmail.com>
2015-12-03 09:36:49 -0500

    switched order of operations, added pip install

--- bin/install.sh
+++ bin/install.sh
@@ -2,6 +2,13 @@
 
 TMP_DIR="/tmp/sample_synth"
 
+if hash virtualenv 2>/dev/null; then
+  echo "virtualenv available"
+else
+  echo >&2 "virtualenv not available. Please install before continuing";
+  exit 1;
+fi
+
 ## Download sms-tools
 echo "Downloading sms-tools"
 mkdir $TMP_DIR
@@ -10,9 +17,7 @@ unzip -q ${TMP_DIR}/master.zip -d $TMP_DIR
 cp -R ${TMP_DIR}/sms-tools-master ./lib/sms-tools
 rm -rf $TMP_DIR
 
-if hash virtualenv 2>/dev/null; then
-  echo "virtualenv available"
-else
-  echo >&2 "virtualenv not available. Please install before continuing";
-  exit 1;
-fi
+source bin/activate
+pip install -r requirements.txt
+(cd lib/sms-tools/software/models/utilFunctions_C && python compileModule.py build_ext --inplace)
+deactivate
