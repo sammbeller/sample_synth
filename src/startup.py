ad567ee77e7264a016f3e3d3caa592837da6d0e8
sbeller <sammbeller@gmail.com>
2016-01-01 15:43:27 -0500

    added harmonic, sinwave

--- src/startup.py
+++ src/startup.py
@@ -4,3 +4,5 @@ import scipy
 import matplotlib.pyplot as plt
 
 stft = imp.load_source('stft', 'src/stft.py')
+harmonic = imp.load_source('harmonic', 'src/harmonic.py')
+sinwave = scipy.sin(scipy.linspace(0,1,44100)*2*scipy.pi*440)
